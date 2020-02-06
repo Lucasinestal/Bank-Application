a6a834ddce397140d3fc880284621a5523065377
lucas_inestal <lucasainestal@gmail.com>
2020-02-06 18:03:01 +0100

    fixed env

--- public/api/classes/connection.php
+++ public/api/classes/connection.php
@@ -1,17 +1,29 @@
 <?php
 
+require_once __DIR__ . '\..\..\..\env\vendor\autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 class Connection
 {
-    private $server = "mysql:host=localhost;dbname=bank;";
-    private $user = "root";
-    private $pass = "";
+    private $db;
+    private $server;
+    private $user;
+    private $pass;
     private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
     protected $con;
 
- 
     
     public function openConnection()
     {
+        $this->user = getenv('DB_USER');
+        $this->db = getenv('DB_DATABASE');
+        $this->host = getenv('DB_HOST');
+        $this->server = "mysql:host=$this->host;dbname=$this->db;";
+
+
         try {
             $this->con = new PDO($this->server, $this->user, $this->pass, $this->options);
             return $this->con;
@@ -20,6 +32,7 @@ class Connection
     }
     public function closeConnection()
     {
+        
         $this->con = null;
     }
 }
